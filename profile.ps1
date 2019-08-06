61a6427871d574f28b4ac9bed365547d2da3ca60
cyy <cyyever@outlook.com>
2019-08-06 10:02:32 +0800

    fix url

--- profile.ps1
+++ profile.ps1
@@ -1,4 +1,4 @@
 $PSReadLineProfilePath=Join-Path -Path  "$PSScriptRoot" -ChildPath "PSReadLineProfile.ps1"
 if (!(Test-Path "$PSReadLineProfilePath")) {
-  Invoke-WebRequest -Uri "https://github.com/PowerShell/PSReadLine/blob/master/PSReadLine/SamplePSReadLineProfile.ps1" -OutFile "$PSReadLineProfilePath"
+  Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PowerShell/PSReadLine/master/PSReadLine/SamplePSReadLineProfile.ps1" -OutFile "$PSReadLineProfilePath"
 }
