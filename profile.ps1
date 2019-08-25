23b903a00fdde47bdb1e9bec0e730eef284bc6cd
cyy <cyyever@outlook.com>
2019-08-25 21:45:48 +0800

    整理配置

--- profile.ps1
+++ profile.ps1
@@ -6,3 +6,7 @@ if (!(Test-Path "$PSReadLineProfilePath")) {
 if (!(Get-Module -ListAvailable -Name z)) {
  start-process -wait -verb runAs -argumentlist "Install-Module -AllowClobber -Name z"
 }
+
+if (powershell.exe -File  (Join-Path -Path  "$PSScriptRoot" -ChildPath "eink.ps1")) {
+  $env:eink_screen=1
+}
