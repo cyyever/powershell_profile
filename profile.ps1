2ce6f5491441e9c54945b9b7ff9089bf28b67cf9
cyy <cyyever@outlook.com>
2019-11-08 15:02:34 +0800

    add profile

--- profile.ps1
+++ profile.ps1
@@ -19,3 +19,11 @@ if($?) {
 if ((Get-Command nvim)) {
   Set-Alias -Name vim -Value nvim
 }
+
+if ((Test-Path "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\profiles.json")) {
+  if($env:eink_screen) {
+    Get-Content $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\profiles.json | %{$_ -replace "^#(.*colorScheme.*:.*eink.*)$", "$1"} | Out-File -Encoding $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\profiles.json
+  } else {
+    Get-Content $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\profiles.json  | %{$_ -replace "^.*colorScheme.*:.*eink.*$", "#$&"} | Out-File -Encoding $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\profiles.json
+  }
+}
