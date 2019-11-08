fbedd989acf6d6fb33f8e469fd02c85e6697b25e
cyy <cyyever@outlook.com>
2019-11-08 13:19:34 +0800

    source PSRead

--- profile.ps1
+++ profile.ps1
@@ -3,12 +3,15 @@ if (!(Test-Path "$PSReadLineProfilePath")) {
   Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PowerShell/PSReadLine/master/PSReadLine/SamplePSReadLineProfile.ps1" -OutFile "$PSReadLineProfilePath"
 }
 
+if ((Test-Path "$PSReadLineProfilePath")) {
+  . "$PSReadLineProfilePath"
+}
+
 if (!(Get-Module -ListAvailable -Name z)) {
   Install-Module -AllowClobber -Scope CurrentUser -Name z
 }
 
 $env:Path += ";${HOME}/opt/bin"
-
 powershell.exe -NoProfile -File (Join-Path -Path "$PSScriptRoot" -ChildPath "eink.ps1")
 if($?) {
   $env:eink_screen=1
