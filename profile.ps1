d7613dfd43283169de48d07c862e2b745be47297
cyy <cyyever@outlook.com>
2022-09-17 07:11:47 +0800

    fix code

--- profile.ps1
+++ profile.ps1
@@ -1,12 +1,3 @@
-$PSReadLineProfilePath = Join-Path -Path "$PSScriptRoot" -ChildPath "PSReadLineProfile.ps1"
-if (!(Test-Path "$PSReadLineProfilePath")) {
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PowerShell/PSReadLine/master/PSReadLine/SamplePSReadLineProfile.ps1" -OutFile "$PSReadLineProfilePath"
-}
-
-if ((Test-Path "$PSReadLineProfilePath")) {
-    . "$PSReadLineProfilePath"
-}
-
 $env:Path = "${HOME}/opt/bin;" + $env:Path
 
 if (!$env:eink_screen) {
