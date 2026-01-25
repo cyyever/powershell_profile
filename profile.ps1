507ab7cd66f07cad8ebc47e9ebbbbbad94507734
cyy <cyyever@outlook.com>
2026-01-25 11:15:12 +0800

    More fixes
    Signed-off-by: cyy <cyyever@outlook.com>

--- profile.ps1
+++ profile.ps1
@@ -1,95 +1,54 @@
-$env:Path = "${HOME}/opt/bin;${HOME}/.cargo/bin;" + $env:Path
+# Add custom paths
+# Install tools: winget install Starship.Starship
+$env:Path = "${HOME}/opt/bin;${HOME}/.cargo/bin;$env:LOCALAPPDATA\Microsoft\WinGet\Packages\Starship.Starship_Microsoft.Winget.Source_8wekyb3d8bbwe;" + $env:Path
 
-if (!$env:eink_screen) {
-    function Decode {
-        If ($args[0] -is [System.Array]) {
-            return [System.Text.Encoding]::ASCII.GetString($args[0])
-        }
-        return ""
-    }
-    #"""Get-WmiObject Win32_DesktopMonitor
-    ForEach ($Monitor in Get-CimInstance -Namespace root\wmi -ClassName WmiMonitorId) {
-        if ((Decode $Monitor.UserFriendlyName -notmatch 0).contains("Paperlike")) {
-            $env:eink_screen = 1
-            break
-        }
-    }
-}
+# Starship prompt - Config: ~/.config/starship.toml
+Invoke-Expression (&starship init powershell)
 
-if ($env:eink_screen) {
-    'ContinuationPrompt', 'Emphasis', 'Error', 'Selection', 'Default', 'Comment', 'Keyword', 'String', 'Operator', 'Variable', 'Command', 'Parameter', 'Type', 'Number', 'Member', 'InlinePrediction', 'ListPrediction', 'ListPredictionSelected' | ForEach-Object { Set-PSReadLineOption -Colors @{ $_ = [ConsoleColor]::Black } }
-}
-else {
-    if ((Get-Command oh-my-posh -ErrorAction SilentlyContinue)) {
-        oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/peru.omp.json" | Invoke-Expression
-    }
-}
+# Aliases
+Set-Alias -Name vim -Value nvim
 
-$ms_terminal_profile = "$env:LocalAppData\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json"
-if ((Test-Path $ms_terminal_profile)) {
-    $old_scheme = 'eink'
-    $new_scheme = 'Gruvbox Dark'
-    if ($env:eink_screen) {
-        $old_scheme = 'Gruvbox Dark'
-        $new_scheme = 'eink'
-    }
-    if (!( Select-String -Path $ms_terminal_profile -Pattern "^(.*colorScheme.*:.*)${new_scheme}(.*)$")) {
-        $tmp = New-TemporaryFile
-        Get-Content $ms_terminal_profile | ForEach-Object { $_ -replace "^(.*colorScheme.*:.*)${old_scheme}(.*)$", ('$1' + $new_scheme + '$2') } | Out-File -Encoding utf8  -FilePath $tmp.FullName
-        cp -Force  $tmp.FullName $ms_terminal_profile
-        Remove-Variable tmp
-    }
-}
-
-if ((Get-Command nvim -ErrorAction SilentlyContinue)) {
-    Set-Alias -Name vim -Value nvim
-}
-
-if ((Get-Command git -ErrorAction SilentlyContinue)) {
-  Import-Module posh-git
-  git config --list -f ~/.gitconfig | ForEach-Object {
-    git config --global $_
-  }
-}
-
-if ((Get-Command lua5.1.exe -ErrorAction SilentlyContinue)) {
-    if ((Test-Path "${HOME}/opt/z.lua")) {
-        iex ($(lua5.1.exe "${HOME}/opt/z.lua/z.lua" --init powershell) -join "`n")
-    }
-}
 
 function pyinstall {
-    if ((Test-Path pyproject.toml)) {
+    if (Test-Path pyproject.toml) {
         python -m pip install --no-build-isolation . --user --force
-    }
-    else {
+    } else {
         python setup.py build_ext --inplace
         python setup.py install --force --user
     }
 }
 
-function pytest {
-    python -m pytest
-}
-
+function pytest { python -m pytest $args }
 
 function pycoverage_run {
-    if (Test-Path $home/opt/cli_tool_configs/coveragerc -PathType leaf) {
-        python -m coverage run --concurrency=multiprocessing --rcfile=$home/opt/cli_tool_configs/coveragerc -m pytest --capture=tee-sys
-    }
-    else {
-        python -m coverage run --concurrency=multiprocessing -m pytest --capture=tee-sys
-    }
+    $rcfile = "$home/opt/cli_tool_configs/coveragerc"
+    $rcflag = if (Test-Path $rcfile) { "--rcfile=$rcfile" } else { "" }
+    python -m coverage run --concurrency=multiprocessing $rcflag -m pytest --capture=tee-sys
     python -m coverage combine
     python -m coverage report
     python -m coverage html
 }
 
-
+# Texlive
 if (Test-Path C:\texlive\2025\bin\win32) {
     $env:Path = "C:/texlive/2025/bin/win32;" + $env:Path
 }
 
+# Fish-like PSReadLine settings
 Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+
+# Fish-like keybindings
+Set-PSReadLineKeyHandler -Chord RightArrow -Function ForwardChar
 Set-PSReadLineKeyHandler -Chord Ctrl+RightArrow -Function AcceptNextSuggestionWord
+Set-PSReadLineKeyHandler -Chord Ctrl+f -Function AcceptSuggestion
+Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Chord UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Chord DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Chord Ctrl+d -Function DeleteCharOrExit
+Set-PSReadLineKeyHandler -Chord Ctrl+w -Function BackwardDeleteWord
+Set-PSReadLineKeyHandler -Chord Alt+d -Function DeleteWord
+Set-PSReadLineKeyHandler -Chord Ctrl+u -Function BackwardDeleteLine
+Set-PSReadLineKeyHandler -Chord Ctrl+k -Function ForwardDeleteLine
